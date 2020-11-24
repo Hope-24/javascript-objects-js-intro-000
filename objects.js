e44af001f3e38852715e33c9242290382369e9ba
Rija Siddiqui <kakuhid@gmail.com>
2020-11-24 01:14:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistTitle) {
-  delete playlist[artistTitle]
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
   return playlist
 }
